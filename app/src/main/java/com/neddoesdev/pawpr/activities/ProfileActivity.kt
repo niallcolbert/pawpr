63354146c85f5b90259503851cbea599d6b56c35
Niall Colbert <niallcolbert87@gmail.com>
2021-05-03 13:45:23 +0100

    Pawfile accepts and displays image

--- app/src/main/java/com/neddoesdev/pawpr/activities/ProfileActivity.kt
+++ app/src/main/java/com/neddoesdev/pawpr/activities/ProfileActivity.kt
@@ -1,15 +1,20 @@
 package com.neddoesdev.pawpr.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_profile.*
+import androidx.appcompat.app.AppCompatActivity
 import com.neddoesdev.pawpr.R
+import com.neddoesdev.pawpr.helpers.readImage
+import com.neddoesdev.pawpr.helpers.showImagePicker
 import com.neddoesdev.pawpr.main.MainApp
 import com.neddoesdev.pawpr.models.ProfileModel
+import kotlinx.android.synthetic.main.activity_profile.*
 import org.jetbrains.anko.toast
+
 
 class ProfileActivity : AppCompatActivity() {
     var profile = ProfileModel()
+    val IMAGE_REQUEST = 1
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +28,24 @@ class ProfileActivity : AppCompatActivity() {
             }
             else {
                 toast("Please enter a name")
+            }
+        }
+
+        chooseImage.setOnClickListener {
+            chooseImage.setOnClickListener {
+                showImagePicker(this, IMAGE_REQUEST)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            IMAGE_REQUEST -> {
+                if (data != null) {
+                    profile.profileImage = data.getData().toString()
+                    profileImage.setImageBitmap(readImage(this, resultCode, data))
+                }
             }
         }
     }
--- app/src/main/java/com/neddoesdev/pawpr/models/ProfileModel.kt
+++ app/src/main/java/com/neddoesdev/pawpr/models/ProfileModel.kt
@@ -1,10 +1,12 @@
 package com.neddoesdev.pawpr.models
 
 data class ProfileModel (
+        var profileImage: String = "",
         var name: String = "",
         var breed: String = "",
         var bio: String = "",
-        var gender: String = "",
+        var gender: String = "male",
+        var size: String = "s",
         var isPuppy: Boolean = false,
         var isFixed: Boolean = false
     )
