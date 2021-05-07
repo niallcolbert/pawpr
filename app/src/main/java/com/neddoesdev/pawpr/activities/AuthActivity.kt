904ad86fac683ff69ebeeaf86f43bf6a011f56ac
Niall Colbert <niallcolbert87@gmail.com>
2021-05-07 12:56:06 +0100

    WIP integrate email/password auth WIP integrate google auth Change Icon

---
+++ app/src/main/java/com/neddoesdev/pawpr/activities/AuthActivity.kt
@@ -0,0 +1,83 @@
+package com.neddoesdev.pawpr.activities
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.neddoesdev.pawpr.R
+import com.neddoesdev.pawpr.main.MainApp
+import kotlinx.android.synthetic.main.activity_auth.*
+
+class AuthActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var app: MainApp
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+        app = application as MainApp
+        // Buttons
+        emailSignInButton.setOnClickListener(this)
+        emailCreateAccountButton.setOnClickListener(this)
+        signOutButton.setOnClickListener(this)
+//        verifyEmailButton.setOnClickListener(this)
+
+        app.auth = FirebaseAuth.getInstance()
+
+        sign_in_button.setSize(SignInButton.SIZE_WIDE)
+        sign_in_button.setColorScheme(0)
+        sign_in_button.setOnClickListener(this)
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = app.auth.currentUser
+    }
+
+    private fun createAccount(email: String, password: String) {
+        app.auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = app.auth.currentUser
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun signIn(email: String, password: String) {
+        app.auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = app.auth.currentUser
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+                if (!task.isSuccessful) {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+           }
+    }
+
+    private fun signOut() {
+        app.auth.signOut()
+    }
+
+    override fun onClick(v: View) {
+        val i = v.id
+        when (i) {
+            R.id.emailCreateAccountButton -> createAccount(fieldSignupEmail.text.toString(), fieldSignupPassword.text.toString())
+            R.id.emailSignInButton -> signIn(fieldLoginEmail.text.toString(), fieldLoginPassword.text.toString())
+            R.id.signOutButton -> signOut()
+        }
+    }
+}
--- app/src/main/java/com/neddoesdev/pawpr/main/MainApp.kt
+++ app/src/main/java/com/neddoesdev/pawpr/main/MainApp.kt
@@ -1,8 +1,13 @@
 package com.neddoesdev.pawpr.main
 
 import android.app.Application
+import com.google.firebase.auth.FirebaseAuth
 
 class MainApp : Application() {
+
+    // [START declare_auth]
+    lateinit var auth: FirebaseAuth
+    // [END declare_auth]
 
     override fun onCreate() {
         super.onCreate()
